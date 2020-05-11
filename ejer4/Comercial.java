2bbf7247292a56b6ce40124e55f19c62cd4dcd32
Rodrigo López Ramos <55782190+rodrigolopezramoss@users.noreply.github.com>
2020-05-11 09:36:42 +0200

    Update Comercial.java

--- ejer4/Comercial.java
+++ ejer4/Comercial.java
@@ -24,82 +24,43 @@ public class Comercial extends Persona{
     return cadena;
   }
   
-
-//  public void vende(Articulo art) {
-//	  articulos.add(new Articulo(art.getNombre(), art.getCantidad())); 
-//  }
   
   public void añadir(Articulo art) {
-	articulos.add(new Articulo(art.getNombre(), art.getCantidad()));
+    articulos.add(new Articulo(art.getNombre(), art.getCantidad()));
   }
   
-//  public void vende(Articulo art) {
-//	int contador = 0;
-//	int can = 0;
-//	
-//	for (Articulo aux : articulos) {
-//	   contador++;
-//	}
-//	
-//	if (contador==0) {
-////		articulos.add(new Articulo(art.getNombre(), art.getCantidad()));
-//		añadir(art);
-//		System.out.println("1");
-//	} else if (contador != 0){
-//	  for (Articulo aux : articulos) {
-//	    can = aux.getCantidad();
-//	    if(art.equals(aux)){
-//	      aux.setCantidad(can += art.getCantidad());
-//	      System.out.println("2.1");
-//	    } else {
-//// 	      articulos.add(new Articulo(art.getNombre(), art.getCantidad()));
-//	   	  System.out.println("2.2");
-//	    }
-//	  }
-//	  
-//	} else {
-////		articulos.add(new Articulo(art.getNombre(), art.getCantidad()));
-//		añadir(art);
-//		System.out.println("3");
-//	}
-//  }
   public void vende(Articulo art) {
-	int estado = 0;
-	int can = 0;
-	int contador = 0;
+    int estado = 0;
+    int can = 0;
+    int contador = 0;
 	
-	for (Articulo aux : articulos) {
-	  contador++;
-	}
+    for (Articulo aux : articulos) {
+      contador++;
+    }
 	
-	if(contador == 0) {
-	  estado = 1;
-	}
-	for (Articulo aux : articulos) {
-	  if(art.equals(aux)){
-	    estado = 2;    
-	  }else {
-	    estado = 1;
-	  }
-	}
+    if(contador == 0) {
+      estado = 1;
+    }
+    for (Articulo aux : articulos) {
+      if(art.equals(aux)){
+        estado = 2;    
+      }else {
+        estado = 1;
+      }
+    }
 	
-	switch(estado) {
-	  case 1:
-	    añadir(art);
-	    break;
-	  case 2:
-	    for (Articulo aux : articulos) {
-	      can = aux.getCantidad();
-	      if(art.equals(aux)){
-	        aux.setCantidad(can += art.getCantidad());
-	      }   
-	    }
-	    break;
-	}
-	  
-	  
-	  
-	  
-	  
+    switch(estado) {
+      case 1:
+        añadir(art);
+        break;
+      case 2:
+        for (Articulo aux : articulos) {
+          can = aux.getCantidad();
+          if(art.equals(aux)){
+            aux.setCantidad(can += art.getCantidad());
+          }   
+        }
+        break;
+    }	  
   }
 }
